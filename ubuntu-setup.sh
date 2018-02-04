b17ae044709fa5434861d9e48049c5b559f3b4fe
Mike Prince <mikeprince13@users.noreply.github.com>
2018-02-04 10:27:56 +0000

    Fixed output notice formatting

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -18,7 +18,7 @@ divider() {
 # Output a notice / section title
 notice() {
   divider
-  printf "\n${RED}$@${NOCOLOR}\n"
+  printf "\n${RED}$1${NOCOLOR}\n"
 }
 
 main() {
@@ -32,44 +32,44 @@ main() {
   readonly NOCOLOR="\033[0m"
 
   # Prompt user for var data
-  notice USER
+  notice "USER"
   read -p 'Username: ' uname
   read -s -p 'Password: ' pword
 
-  notice MYSQL ROOT
+  notice "MYSQL ROOT"
   read -s -p 'MySQL root password: ' dbpword
 
-  notice MYSQL ADMIN
+  notice "MYSQL ADMIN"
   read -p 'MySQL admin username: ' dbuser
   read -s -p 'MySQL admin password: ' dbuserpword
   divider
 
   # Secure SSH
-  notice SECURING SSH
+  notice "SECURING SSH"
   sed -i 's/X11Forwarding yes/X11Forwarding no/' /etc/ssh/sshd_config
   sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
   sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
 
   # Enable UFW, allow SSH
-  notice ENABLING UFW
+  notice "ENABLING UFW"
   ufw allow 22/tcp
   ufw --force enable
 
   # Set locale
-  notice SETTING LOCALE
+  notice "SETTING LOCALE"
   locale-gen en_GB.UTF-8
   printf 'LANG="en_GB.UTF-8"' > /etc/default/locale
   . /etc/default/locale
 
   # Full upgrade
-  notice UPGRADING
+  notice "UPGRADING"
   apt update
   apt full-upgrade -y --force-yes -qq
   apt autoremove -y
   apt-get autoclean
 
   # Enable Swap, size = 1Gb, swappiness = 10
-  notice CREATING SWAP
+  notice "CREATING SWAP"
   fallocate -l 1G /swapfile
   chmod 600 /swapfile
   mkswap /swapfile
@@ -78,7 +78,7 @@ main() {
   echo "vm.swappiness=10" >> /etc/sysctl.conf
 
   # Create user account
-  notice CREATING USER ACCOUNT
+  notice "CREATING USER ACCOUNT"
   adduser --disabled-password --gecos "" $uname
   echo "${uname}:${pword}" | sudo chpasswd
   usermod -aG sudo $uname
@@ -89,12 +89,12 @@ main() {
   chown -R $uname:$uname /home/$uname/.ssh
 
   # Install Fail2Ban
-  notice INSTALLING FAIL2BAN
+  notice "INSTALLING FAIL2BAN"
   apt install -y fail2ban
   curl ${URL}/fail2ban.conf -o /etc/fail2ban/jail.local
 
   # Install MariaDB
-  notice INSTALLING MARIADB
+  notice "INSTALLING MARIADB"
   apt install -y mariadb-server
   # Replicate mysql_secure_installation
   echo "Securing MySQL"
@@ -108,7 +108,7 @@ main() {
   mysql -e "FLUSH PRIVILEGES;"
 
   # Install Nginx
-  notice INSTALLING NGINX
+  notice "INSTALLING NGINX"
   apt install -y nginx
   ufw allow 80/tcp
   ufw allow 443/tcp
@@ -119,7 +119,7 @@ main() {
   mkdir /srv/www
 
   # Get real IP from CloudFlare
-  notice CONFIGURING NGINX FOR CLOUDFLARE
+  notice "CONFIGURING NGINX FOR CLOUDFLARE"
   touch /etc/nginx/snippets/cloudflare.conf
   bash -c "echo '# CloudFlare IPs' > /etc/nginx/snippets/cloudflare.conf"
   bash -c "curl https://www.cloudflare.com/ips-v4 >> /etc/nginx/snippets/cloudflare.conf"
@@ -130,23 +130,23 @@ main() {
   bash -c "echo 'real_ip_header CF-Connecting-IP;' >> /etc/nginx/snippets/cloudflare.conf"
 
   # Install PHP
-  notice INSTALLING PHP
+  notice "INSTALLING PHP"
   add-apt-repository -y ppa:ondrej/php
   apt update
   apt install -y php7.1-fpm php7.1-curl php7.1-gd php7.1-json php7.1-mbstring php7.1-mcrypt php7.1-mysql php7.1-xml php7.1-zip
 
   # Install Sendmail
-  notice INSTALLING SENDMAIL
+  notice "INSTALLING SENDMAIL"
   apt install -y sendmail
 
   # Git
-  notice INSTALLING GIT
+  notice "INSTALLING GIT"
   apt install -y git git-core
   mkdir /srv/git
   chown $uname:$uname /srv/git
 
   # Install certbot
-  notice INSTALLING CERTBOT
+  notice "INSTALLING CERTBOT"
   add-apt-repository ppa:certbot/certbot
   apt update
   apt install -y python-certbot-nginx
@@ -154,7 +154,7 @@ main() {
   chmod +x /etc/cron.daily/cerbot-renew
 
   # Setup SSL
-  notice CONFIGURING SSL CERTS
+  notice "CONFIGURING SSL CERTS"
   openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
   curl ${URL}/ssl-params.conf -o /etc/nginx/snippets/ssl-params.conf
 
@@ -168,7 +168,7 @@ main() {
   # Truncate logs
   find /var/log -type f -exec truncate -c -s0 {} \;
 
-  notice SETUP COMPLETE
+  notice "SETUP COMPLETE"
 }
 
 main
